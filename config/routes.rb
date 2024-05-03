e82787950c0457acc22b2baf2ac039b0637cb901
dungtm-1343 <tran.manh.dung@sun-asterisk.com>
2024-05-03 14:50:09 +0700

    Chapter 6

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root "static_pages#home"
   get "contact", to: "static_pages#contact"
+
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  resources :users, only: %i(new create show)
 end
